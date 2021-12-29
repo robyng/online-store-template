35a768d6afe99f4bcca4a8563a2b792014189e61
robyng <robyngraham01@gmail.com>
2021-12-29 11:38:42 -0800

    add associations and includes to routes to join tags and categories to products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll()
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      { 
+        model: Category, 
+        attributes: ['id', 'category_name']
+      },
+    
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      
+    ]
+  })
 .then(dbProduct => res.json(dbProduct))
 .catch(err => {
     console.log(err);
@@ -22,7 +36,20 @@ router.get('/:id', (req, res) => {
   Product.findOne({
     where: {
         id: req.params.id
-    }
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      { 
+        model: Category, 
+        attributes: ['id', 'category_name']
+      },
+    
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      
+    ]
 })
 .then(dbProduct => {
     if (!dbProduct) {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,16 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll()
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      { 
+        model: Product, 
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+      
+    ]
+  })
   .then(dbTags => res.json(dbTags))
   .catch(err => {
     console.log(err);
@@ -21,7 +30,15 @@ router.get('/:id', (req, res) => {
   Tag.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      { 
+        model: Product, 
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+      
+    ]
   })
   .then(dbTags => {
     if(!dbTags) {
